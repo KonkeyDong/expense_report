8d3887a3ea833e71e1bf74528735262de242b8e2
konkeydong <dittoes.1@gmail.com>
2021-01-26 14:27:08 -0600

    add expenses unit tests

--- node/src/sample.ts
+++ node/src/sample.ts
@@ -1,15 +1,21 @@
 
 import {MerchantType} from './merchant_type';
+import {Expenses} from './expenses';
+import {Merchant} from './merchant';
 
 // const { Pool } = pkg;
 async function main() {
-  const merchantType = new MerchantType();
+  const expenses = new Expenses();
+  const data = await expenses.select(1);
+  // console.log('type: ', typeof purchase_date, '  |  data: ', purchase_date);
+  console.log('data: ', data);
+  // const merchantType = new MerchantType();
   // const x = await merchantType.select(1);
-  const x = await merchantType.selectAll();
+  // const x = await merchantType.selectAll();
 
   // const x = await merchantType.selectAll();
   // const x = await merchantType.update(12, 'clam');
-  console.log('result: ', x);
+  // console.log('result: ', x);
 
   // merchantType.finish();
 }
